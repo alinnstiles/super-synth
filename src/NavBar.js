6e5cc610e99b8513368e7010b16e4017fcbedaaa
Stephin Roberts <stephin.roberts@gmail.com>
2024-05-21 14:10:54 -0400

    Updated NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom'
+import './NavBar.css';
 
 function NavBar() {
 
     return (
-        <div >
-            <h4>
-            <Link to="/">Home</Link>
+        <div class="scrollmenu">
+                <a href="/" class="active">Home</a>
+                <a href="/record">Record</a>
+                <a href="/myrecordings">My Recordings</a>
+                <a href="commsounds">Community Sounds</a>
+            {/* <Link to="/">Home</Link>
             &nbsp; &nbsp;
             <Link to="/record">Record</Link>
             &nbsp; &nbsp;
             <Link to="/myrecordings">My Recordings</Link>
             &nbsp; &nbsp;
-            <Link to="/commsounds">Community Sounds</Link>
-            </h4>
+            <Link to="/commsounds">Community Sounds</Link> */}
         </div>
     )
 }
