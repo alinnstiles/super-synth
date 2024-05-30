e3e8f45078f13a7aa38882ed78e579fda0038dde
Brandon <brandonslee3@gmail.com>
2024-05-30 12:31:10 -0400

    Flames & User Verification

--- src/Comment.js
+++ src/Comment.js
@@ -2,29 +2,32 @@ import { useState } from "react"
 
 function Comment({current}){
 
-    const [commentLikes, setCommentLikes] = useState(current.likes)
+    const [totalLikes, setTotalLikes] = useState(current.likes)
+    const [commentLikes, setCommentLikes] = useState(false)
 
-    const handleCommentLike = (comment) => {
-        console.log(comment)
-        fetch(`/api/patch-comment/${comment.id}`, {
+    const handleCommentLike = () => {
+        fetch(`/api/patch-comment/${current.id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                "Flames:": comment.likes + 1
+                "likes": totalLikes + (commentLikes ? (-1) : (1))
             })
         })
         .then(res => res.json())
-        .then(promise => setCommentLikes(promise.likes))
+        .then(promise => {
+            setTotalLikes(promise.likes)
+            setCommentLikes(!commentLikes)
+        })
     }
 
     return(
         <div className="comment">
             <p><b>{current.user.username}</b></p>
             <p>{current.comment}</p>
-            <p>{"Flames: " + commentLikes}</p>
-            <button onClick={() => handleCommentLike(current)}>🔥</button>
+            <p>{"Flames: " + totalLikes}</p>
+            <button onClick={handleCommentLike}>{commentLikes ? "Unflame" : "🔥"}</button>
         </div>
     )
 }
