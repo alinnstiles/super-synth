8b5cede3f1580668dfae2b06b45be3e9392a79fb
Stephin Roberts <stephin.roberts@gmail.com>
2024-07-25 22:34:54 -0400

    no updates

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -27,6 +27,7 @@ bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
 
 ######################### USER ############################
 
